672159d5926c51fe9e6508c70b2834984308f169
paprykdev <58005447+paprykdev@users.noreply.github.com>
2023-10-10 00:38:04 +0200

    feat: intro to rewrite css to tailwind

--- packages/ui/card.tsx
+++ packages/ui/card.tsx
@@ -1,4 +1,4 @@
-import * as React from "react";
+import React from "react";
 
 export function Card({
   className,
@@ -18,7 +18,7 @@ export function Card({
       rel="noopener noreferrer"
       target="_blank"
     >
-      <h2>
+      <h2 className="font-bold">
         {title} <span>-&gt;</span>
       </h2>
       <p>{children}</p>
